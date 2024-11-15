a879c4dc3ca325488e31b5f215a0deece7455264
Mendoza-2A <Mendozailadio@gmail.com>
2024-11-15 14:05:25 +0800

    Add files via upload

---
+++ inheritanceSample.java
@@ -0,0 +1,17 @@
+public class inheritanceSample {
+    public static void main(String[] args) {
+       
+        student student = new student("Iladio Mendoza", "Lemery", "BSIT", 2);
+        
+        System.out.println("Student Name: " + student.getName());
+        System.out.println("Student Address: " + student.getAddress());
+        System.out.println("Student Program: " + student.getProgram());
+        System.out.println("Student Year: " + student.getYear());
+
+        staff staff = new staff("Jaycee", "Balayan", "BSU", 25000.0);
+        System.out.println("\nStaff Name: " + staff.getName());
+        System.out.println("Staff Address: " + staff.getAddress());
+        System.out.println("Staff School: " + staff.getSchool());
+        System.out.println("Staff Salary: " + staff.getSalary());
+    }
+}
