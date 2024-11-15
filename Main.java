257cd931e6bedc4fc7ac04b86264baf88dd30a14
Mendoza-2A <Mendozailadio@gmail.com>
2024-11-15 15:09:53 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args) {
+        Shape circle = new Circle(35.0, "Red", true);
+        System.out.println("\n" + "Area of Circle: " + circle.getArea());
+        System.out.println("Perimeter of Circle: " +circle.getPerimeter());
+        System.out.println(circle.toString() + "\n");
+
+        Shape Rectangle = new Rectangle(22.9, 85.0, "Red", true);
+        System.out.println("Area of Rectangle: " + Rectangle.getArea());
+        System.out.println("Perimeter of Rectangle: " + Rectangle.getPerimeter());
+        System.out.println(Rectangle.toString() + "\n");
+        
+        Shape Square = new Square(75.0, 65.0, 30.0, "Red", true);
+        System.out.println("Area of Square: " + Square.getArea());
+        System.out.println("Perimeter of Square: " + Square.getPerimeter());
+        System.out.println(Square.toString());
+    }
+}
